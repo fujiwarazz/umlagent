6daaef85fc3d5900a17f7812a5f2ce1a3c038893
fujiwarazz <peelsannaw@163.com>
2025-05-23 13:12:31 +0800

    add swe agent module framework

--- app/agents/sweagent.py
+++ app/agents/sweagent.py
@@ -5,9 +5,9 @@ from pydantic import Field
 
 from agents.tool_call import ToolCallAgent
 from utils.logger import logger
-from prompts.umlagent import NEXT_STEP_PROMPT, PLANNING_SYSTEM_PROMPT
+from prompts.sweagent import SWE_NEXT_STEP_TEMPLATE, SWE_SYSTEM_PROMPT
 from utils.entity import AgentState, Message, ToolCall
-# from tools import CreateChatCompletion, Terminate, ToolCollection, CodeExcute, Bash, FileSaver,FileSeeker,Github,UML,REASK
+from tools import CreateChatCompletion,Terminate,BaiduSearch
 from tools import Terminate, ToolCollection
 
 
@@ -28,3 +28,28 @@ class SWEAgent(ToolCallAgent):
         )
         if kwargs.get('available_tools') is not None:
             self.available_tools.add_tools(*kwargs.get('available_tools').tools)
+            
+            
+
+class SWEAgent(ToolCallAgent):
+    """An agent that implements the SWEAgent paradigm for executing code and natural conversations."""
+
+    name: str = "swe"
+    description: str = "an software engineering agent that can read and interact with codes."
+
+    system_prompt: str = SWE_SYSTEM_PROMPT
+    next_step_prompt: str = SWE_NEXT_STEP_TEMPLATE
+
+    available_tools: ToolCollection = ToolCollection(
+        Terminate(),BaiduSearch()
+    )
+    special_tool_names: List[str] = Field(default_factory=lambda: [Terminate().name])
+
+    max_steps: int = 30
+
+
+    async def think(self) -> bool:
+        """Process current state and decide next action"""
+        # Update working directory
+        
+        return await super().think()
